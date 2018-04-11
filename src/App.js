cfc9f40fa1f0812363d01aaf8af506eb0bcc33e6
Brack Carmony <brackcarmony@gmail.com>
2018-04-11 17:40:14 -0400

    Production mode enabled.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Header from './components/header/Header';
 import './base.css';
 import './App.css';
 
+import axios from 'axios';
+
+if (process.env.NODE_ENV === 'production'){
+	axios.defaults.baseURL = 'https://houser.devmountain.com/v2/part2/'
+}
+
 class App extends Component {
   render() {
     return (
